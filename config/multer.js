2d789fd858e6083d62ab4f21611eef627f891a3b
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-09-07 15:21:52 +0800

    Refactor: Replace disk storage with memory storage

--- config/multer.js
+++ config/multer.js
@@ -1,15 +1,19 @@
 import multer from "multer";
+import dotenv from "dotenv";
+dotenv.config();
 
-const storage = multer.diskStorage({
-  destination: (req, file, done) => {
-    done(null, "/tmp/my-uploads");
-  },
-  filename: (req, file, done) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    done(null, file.fieldname + "-" + uniqueSuffix);
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB Max file size
+  fileFilter: (req, file, done) => {
+    const allowedTypes = ["image/jpeg", "image/png", "application/pdf"];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return done(new Error("Invalid file type"), false);
+    }
+    done(null, true);
   },
 });
-
-const upload = multer({ storage: storage });
 
 export default upload;
