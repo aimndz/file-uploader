5e9b66b442737ddd3748771652457e4aa398b3b4
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-09-10 21:30:21 +0800

    Feat: Format date

--- middlewares/setHomeData.js
+++ middlewares/setHomeData.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import { formatDistanceToNow } from "date-fns";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -6,8 +7,6 @@ const prisma = new PrismaClient();
 const setHomeData = asyncHandler(async (req, res, next) => {
   // Initialize errors if not already present
   res.locals.errors = res.locals.errors || [];
-
-  console.log(res.locals.errors);
 
   const currentFolder = await prisma.folder.findFirst({
     where: {
@@ -40,9 +39,29 @@ const setHomeData = asyncHandler(async (req, res, next) => {
 
   // Modify each item based on their type
   const items = [
-    ...folders.map((folder) => ({ ...folder, type: "folder" })),
-    ...files.map((file) => ({ ...file, type: "file" })),
+    ...folders.map((folder) => ({
+      ...folder,
+      type: "folder",
+      createdAt: formatDistanceToNow(folder.createdAt, {
+        addSuffix: true,
+      }),
+      updatedAt: formatDistanceToNow(folder.createdAt, {
+        addSuffix: true,
+      }),
+    })),
+    ...files.map((file) => ({
+      ...file,
+      type: "file",
+      createdAt: formatDistanceToNow(folder.createdAt, {
+        addSuffix: true,
+      }),
+      updatedAt: formatDistanceToNow(folder.createdAt, {
+        addSuffix: true,
+      }),
+    })),
   ];
+
+  console.log(items);
 
   res.locals.title = currentFolder ? currentFolder.name : "Home Page";
   res.locals.user = req.user;
